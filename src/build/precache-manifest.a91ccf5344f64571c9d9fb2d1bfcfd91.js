421ed3e7ce747df6b33eb395cf990b48d287d965
reaperofchaos <tokugawa111@gmail.com>
2020-04-28 12:59:26 -0400

    First Commit

---
+++ src/build/precache-manifest.a91ccf5344f64571c9d9fb2d1bfcfd91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8faa4c8dd32ae5060016ff4f2c329ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44dc47cac8173634c519",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "251042b8908d8a8a483a",
+    "url": "/static/js/2.5e324e9f.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.5e324e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44dc47cac8173634c519",
+    "url": "/static/js/main.fb748d6a.chunk.js"
+  },
+  {
+    "revision": "c6cd80a6fce542ddaee9",
+    "url": "/static/js/runtime-main.b9244b71.js"
+  }
+]);
